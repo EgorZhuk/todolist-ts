df4b61ad9f5983b090d0a9df92a44d46950c0306
EgorZhuk <egorbug@gmail.com>
2022-12-05 23:24:31 +0300

    It-Incubator Lesson 1

---
+++ src/Todolist.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type TodolistPropsType={
+    title: string
+    tasks: Array<TasksType>
+}
+
+type TasksType={
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+const Todolist = (props:TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el=>{
+                    return(
+                    <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                    )
+                })}
+
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+export default Todolist
+
