34024a85df7848f7637b0543b4b86e4e8788ceb5
EgorZhuk <egorbug@gmail.com>
2023-04-24 18:33:52 +0300

    decomposing app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,29 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {
-	AppBar,
-	Button,
 	CircularProgress,
 	Container,
-	IconButton,
-	LinearProgress,
-	Toolbar,
-	Typography
 } from '@mui/material';
-import { Menu } from '@mui/icons-material'
 import { Login } from 'features/auth/Login/Login'
 import './App.css'
 import { TodolistsList } from 'features/todolists-list/TodolistsList'
 import { ErrorSnackbar } from 'common/components'
 import { useActions } from 'common/hooks';
-import { selectIsLoggedIn } from 'features/auth/auth.selectors';
-import { selectAppStatus, selectIsInitialized } from 'app/app.selectors';
+import { selectIsInitialized } from 'app/app.selectors';
 import { authThunks } from 'features/auth/auth.reducer';
+import Header from 'common/components/Header/Header';
+import Routing from 'common/components/Routing/Routing';
 
 function App() {
-	const status = useSelector(selectAppStatus)
-	const isInitialized = useSelector(selectIsInitialized)
-	const isLoggedIn = useSelector(selectIsLoggedIn)
 
-	const {initializeApp, logout} = useActions(authThunks)
+	const isInitialized = useSelector(selectIsInitialized)
+
+	const {initializeApp} = useActions(authThunks)
 
 	useEffect(() => {
 		initializeApp({})
 	}, [])
-
-	const logoutHandler = () => logout({})
 
 	if (!isInitialized) {
 		return <div
@@ -45,24 +36,8 @@ function App() {
 		<BrowserRouter>
 			<div className="App">
 				<ErrorSnackbar/>
-				<AppBar position="static">
-					<Toolbar>
-						<IconButton edge="start" color="inherit" aria-label="menu">
-							<Menu/>
-						</IconButton>
-						<Typography variant="h6">
-							News
-						</Typography>
-						{isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
-					</Toolbar>
-					{status === 'loading' && <LinearProgress/>}
-				</AppBar>
-				<Container fixed>
-					<Routes>
-						<Route path={'/'} element={<TodolistsList/>}/>
-						<Route path={'/login'} element={<Login/>}/>
-					</Routes>
-				</Container>
+				<Header/>
+				<Routing/>
 			</div>
 		</BrowserRouter>
 	)
---
+++ src/common/components/Routing/Routing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {TodolistsList} from 'features/todolists-list/TodolistsList';
+import {Login} from 'features/auth/Login/Login';
+import {Container} from '@mui/material';
+
+const Routing = () => {
+  return (
+    <Container fixed>
+      <Routes>
+        <Route path={'/'} element={<TodolistsList/>}/>
+        <Route path={'/login'} element={<Login/>}/>
+      </Routes>
+    </Container>
+  );
+};
+
+export default Routing;
