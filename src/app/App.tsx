da3c124eb96d7f4bed20fa4a3d9172ec5befadf4
EgorZhuk <egorbug@gmail.com>
2023-05-18 01:39:57 +0300

    Deploy Todolist to GitHub Pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import {
 	CircularProgress,
 } from '@mui/material';
@@ -28,13 +28,13 @@ function App() {
 	}
 
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<div className="App">
 				<ErrorSnackbar/>
 				<Header/>
 				<Routing/>
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 
